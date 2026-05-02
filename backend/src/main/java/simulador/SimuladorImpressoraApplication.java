c6e3f11c529dda39b56b8da261f6f26860da31ee
eslainy <eslainy.silva@unifesspa.edu.br>
2026-05-02 15:37:22 -0300

    correção de erro

--- backend/src/main/java/simulador/SimuladorImpressoraApplication.java
+++ backend/src/main/java/simulador/SimuladorImpressoraApplication.java
@@ -2,7 +2,7 @@ package simulador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//najauusuj
+
 @SpringBootApplication
 public class SimuladorImpressoraApplication {
 
