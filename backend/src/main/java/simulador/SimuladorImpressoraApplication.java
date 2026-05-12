87fc598a2a325853b4132f62986d2f8659f9b2dc
eslainy <eslainy.silva@unifesspa.edu.br>
2026-05-12 19:06:50 -0300

    modificação

--- backend/src/main/java/simulador/SimuladorImpressoraApplication.java
+++ backend/src/main/java/simulador/SimuladorImpressoraApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //nj
 //mibjk
+//lkj
 @SpringBootApplication
 public class SimuladorImpressoraApplication {
 
